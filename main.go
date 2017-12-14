39cda0c4684d5d25421b338f3848c7b6a3c63df7
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-12-14 16:30:26 -0500

    Refactored

--- main.go
+++ main.go
@@ -9,19 +9,44 @@ import (
 	"golang.org/x/oauth2"
 )
 
+var client *github.Client
+
 func main() {
-	client := initialize()
-	fmt.Println(client)
+	initialize()
+
+	getRepos()
+	getOrgs()
+	getWithoutAuth()
 }
 
-func initialize() *github.Client {
+func getRepos() {
+	ctx := context.Background()
+	repos, _, err := client.Repositories.List(ctx, "", nil)
+	check(err)
+	fmt.Println(repos)
+}
+
+func getOrgs() {
+	ctx := context.Background()
+	orgs, _, _ := client.Organizations.List(ctx, "", nil)
+	fmt.Println(orgs)
+}
+
+func getWithoutAuth() {
+	ctx := context.Background()
+	client := github.NewClient(nil)
+	repos, _, _ := client.Repositories.List(ctx, "fenwickelliott", nil)
+	fmt.Println(repos)
+}
+
+func initialize() {
 	// save api token locally
 	token, err := ioutil.ReadFile("token")
 	check(err)
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: string(token)})
 	tc := oauth2.NewClient(ctx, ts)
-	return github.NewClient(tc)
+	client = github.NewClient(tc)
 }
 
 func check(err error) {
