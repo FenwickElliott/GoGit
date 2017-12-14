d5ee648afa2ca957c0fe0816a4852772a3b9dcd9
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-12-14 15:21:00 -0500

    Authentication

--- main.go
+++ main.go
@@ -3,17 +3,28 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+
+	"golang.org/x/oauth2"
 
 	"github.com/google/go-github/github"
 )
 
 func main() {
-	client := github.NewClient(nil)
-	ctx := context.Background()
-	orgs, _, err := client.Organizations.List(ctx, "willnorris", nil)
+	// save api token locally as 'token'
+	token, err := ioutil.ReadFile("token")
 	check(err)
 
-	fmt.Println(orgs)
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: string(token)})
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	repos, _, err := client.Repositories.List(ctx, "", nil)
+	check(err)
+
+	fmt.Println(repos)
 }
 
 func check(err error) {
